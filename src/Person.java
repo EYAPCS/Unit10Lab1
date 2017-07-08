9c52f404478acdd31a0c909f54f8eef2c987ca5a
Emma <emmayang78@gmail.com>
2017-07-08 19:26:05 -0400

    Finished get/set methods for Person class

--- src/Person.java
+++ src/Person.java
@@ -9,4 +9,44 @@ public class Person {
     String phoneNumber;
     Date birthDate;
 
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public void setAddress(String newAddress) {
+        address = newAddress;
+    }
+
+    public void setEmailAddress(String newEmailAddress) {
+        emailAddress = newEmailAddress;
+    }
+
+    public void setPhoneNumber(String newPhoneNumber) {
+        phoneNumber = newPhoneNumber;
+    }
+
+    public void setBirthDate(Date newDate) {
+        birthDate = newDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
 }
