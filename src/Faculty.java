b73f33104fddc3869df308c3a61039ec188a1c90
Emma <emmayang78@gmail.com>
2017-07-09 10:46:35 -0400

    Added Faculty class

---
+++ src/Faculty.java
@@ -0,0 +1,33 @@
+/**
+ * Created by emma on 7/9/17.
+ */
+public class Faculty extends Employee {
+
+    private String officeHours;
+    private String rank;
+
+    public Faculty(String name, String address, String emailAddress, String phoneNumber, Date birthDate, double salary, Date hireDate, String officeHours, String rank) {
+        super(name, address, emailAddress, phoneNumber, birthDate, salary, hireDate);
+
+        setOfficeHours(officeHours);
+        setRank(rank);
+
+    }
+
+    public String getOfficeHours() {
+        return officeHours;
+    }
+
+    public void setOfficeHours(String officeHours) {
+        this.officeHours = officeHours;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+}
